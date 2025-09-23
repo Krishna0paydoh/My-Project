1feeddf1c53ddb286128abb8fdc6997388690746
DESKTOP-BI2FM0M\Admin <krishna@paydoh.money>
2025-09-23 18:25:14 +0530

    objects & Destructuring

--- Objects & Destructuring.js
+++ Objects & Destructuring.js
@@ -11,12 +11,37 @@ console.clear();
 // console.log(student);
 
 
-const car = { brand: "Tesla", model: "Model 3", year: 2023 };
+// const car = { brand: "Tesla", model: "Model 3", year: 2023 };
 
-for (let key in car) {
-    console.log(key, car[key]);
-}
+// for (let key in car) {
+//     console.log(key, car[key]);
+// }
 
-console.log(Object.keys(car));
-console.log(Object.values(car));
-console.log(Object.entries(car));
+// console.log(Object.keys(car));
+// console.log(Object.values(car));
+// console.log(Object.entries(car));
+
+// const user =
+// {
+//     username: "chishiya",
+//     email: "chishiya123@xyzmail.com",
+//     password: "securepassword"
+// };
+
+// const { username, email } = user;
+// console.log([{ username, email }]);
+
+
+const product = {
+    id: 101,
+    name: "Laptop",
+    price: 999.99,
+    specs: {
+        cpu: "Intel i7",
+        ram: "16GB",
+        storage: "512GB SSD"
+    }
+};
+
+const { name, price, specs: { cpu, ram } } = product;
+console.log(product);
